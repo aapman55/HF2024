5696f93a2080ff398a4b1117ab1582dbc689f6df
aapman55 <zhi-li_liu@live.nl>
2024-03-02 22:55:00 +0100

    restructure folders

--- hf_bi_python_exercise/main.py
+++ hf_bi_python_exercise/main.py
@@ -2,10 +2,10 @@ from pathlib import Path
 from sys import path as syspath
 
 # Just to be sure that the utils can be found
-syspath.append(Path(".").parent.parent.as_posix())
+syspath.append(Path(".").parent.as_posix())
 
 if __name__ == "__main__":
-    from hf2024.io.json import download_json_data
+    from hf_bi_python_exercise.io.json import download_json_data
 
     file_url = "https://bnlf-tests.s3.eu-central-1.amazonaws.com/recipes.json"
     download_json_data(file_url, "./bi_recipes.json")
