8e834a5d271effce47be715525714e5a20738c0e
aapman55 <zhi-li_liu@live.nl>
2024-03-03 11:26:25 +0100

    tests for module json

---
+++ tests/hf_bi_python_exercise/io/test_json.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+import unittest
+from unittest import mock
+
+from hf_bi_python_exercise.io.json import download_json_data
+
+
+@dataclass
+class DummyResponse:
+    text: str
+
+
+class TestJson(unittest.TestCase):
+
+    @mock.patch("hf_bi_python_exercise.io.json.requests")
+    @mock.patch("hf_bi_python_exercise.io.json.Path")
+    def test_download_json_data(self, mock_path, mock_requests) -> None:
+        """
+        The used functions are all standard libraries.
+        So what we can test here is whether the calls and outputs are connected correctly
+        :param mock_path:
+        :param mock_requests:
+        :return:
+        """
+        mock_requests.get.return_value = DummyResponse(text="{}")
+
+        download_json_data(url="dummy_url", download_file_path="dummy_path")
+
+        mock_requests.get.assert_called_with("dummy_url")
+        mock_path.assert_called_with("dummy_path")
+        mock_path.return_value.write_text.assert_called_with("{}")
